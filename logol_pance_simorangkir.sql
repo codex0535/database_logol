e0ff7c250793eead14fad891cce7208939ed655d
pance.s <pance.simorangkir@danamon.co.id>
2021-03-16 13:16:39 +0700

    change dataType of id to INT and make it  auto_increment

--- logol_pance_simorangkir.sql
+++ logol_pance_simorangkir.sql
@@ -4,20 +4,18 @@ CREATE DATABASE logol_pance_simorangkir;
 USE logol_pance_simorangkir;
 
 CREATE TABLE `logol_pance_simorangkir`.`news` (
-  `id` VARCHAR(10) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `title` VARCHAR(100) NOT NULL,
   `description` TEXT(1000) NOT NULL,
-  `created_date` DATETIME DEFAULT NOW(),
+  `created_date` DATETIME NOT NULL,
   PRIMARY KEY (`id`));
 
 INSERT INTO `logol_pance_simorangkir`.`news`
-(id,
-title,
+(title,
 description,
 created_date)
 VALUES
-('b1',
-'About Ronaldo',
+('About Ronaldo',
 'Cristiano Ronaldo lahir di Funchal, Madeira, Portugal pada 5 Pebruari 1985. Pemain bernama lengkap Cristiano Ronaldo dos Santos Aveiro itu memegang rekor sebagai pemain termahal di dunia ketika ditransfer Real Madrid dari Manchester United pada musim panas 2009 senilai 132 juta dolar atau lebih dari 1,3 triliun rupiah.',
 NOW());
 
